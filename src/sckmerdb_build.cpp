5525f43ff173d1787d4ee135fd4f59c33d7212ea
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2019-08-16 17:32:12 -0700

    add second input to sckmer builddb (monospecific SNPs whitelists) (#14)
    * Autosave commit created on Mon Jul 22 12:24:29 PDT 2019.
    
    * Autosave commit created on Tue Jul 23 09:48:49 PDT 2019.
    
    * Autosave commit created on Tue Jul 23 13:39:17 PDT 2019.

--- src/sckmerdb_build.cpp
+++ src/sckmerdb_build.cpp
@@ -7,10 +7,12 @@
 #include <cstring>
 #include <mutex>
 #include <thread>
+#include <regex>
 
 #include <fcntl.h>
 #include <unistd.h>
 #include <assert.h>
+#include <libgen.h>
 
 using namespace std;
 
@@ -71,6 +73,35 @@ uint64_t seq_encode(const char* buf) {
     return seq_code;
 }
 
+string profile_path(string fname) {
+    // fname must be DDDDDD.sckmer_allowed.tsv
+    // result would be DDDDDD.sckmer_profiles.tsv
+    string base = basename(const_cast<char*>(fname.c_str()));
+    string dir = dirname(const_cast<char*>(fname.c_str()));
+    string altbase = regex_replace(base, regex("\\b([1-9][0-9][0-9][0-9][0-9][0-9]).sckmer_allowed.tsv$"), "$1.sckmer_profiles.tsv");
+    if (altbase.length() != strlen("DDDDDD.sckmer_profiles.tsv") || base == altbase) {
+        cerr << "Malformed argument: " << base << endl;
+        cerr << "Required format: DDDDDD.sckmer_allowed.tsv" << endl;
+        cerr << "Additional details: " << altbase << endl;
+        assert(false);
+    }
+    if (dir == ".") {
+        return altbase;
+    }
+    return dir + "/" + altbase;
+}
+
+inline uint64_t get_kmer(const KmerData& kmer_data) {
+    return get<0>(kmer_data);
+}
+
+inline uint64_t get_snp_with_rc_and_offset(const KmerData& kmer_data) {
+    return get<1>(kmer_data);
+}
+
+inline uint64_t get_snp(const KmerData& kmer_data) {
+    return get<1>(kmer_data) >> 8;
+}
 
 // INPUT:  A sckmers.tsv file with content like:
 //
@@ -89,13 +120,15 @@ uint64_t seq_encode(const char* buf) {
 //      $6 - reverse complement of $4
 //      $7 - unused
 //      $8 - unused
-//      $9 - 6 decimal digit species ID
-//      $10 - unused
+//      $9 - unused
+//      $10 - 6 decimal digit species ID
+//      $11 - unused
+//      $12 - unused
 //
 //  a "snp coordinate" is obtained by concatenating the following character strings and
 //  converting the result back to an integer:
 //
-//      * the 6 decimal digit species id (input column $8),
+//      * the 6 decimal digit species id (input column $10),
 //
 //      * a single digit allele type:
 //           (0 = major allele, matching input $3 and $5,
@@ -106,16 +139,27 @@ uint64_t seq_encode(const char* buf) {
 
 
 
-void bit_load(const char* k_path, vector<KmerData>& result) {
+void bit_load(string f_allowed, vector<KmerData>& result, vector<KmerData>& profiles) {
 
-    assert(k_path);
-    FILE *fp = fopen(k_path, "r");
+    auto t_start = chrono_time();
+
+    string f_profile = profile_path(f_allowed);
+
+    // String addition so hopefully this << is atoimic.
+    cerr << (string("Loading files ") + f_allowed + " and " + f_profile + "\n");
+
+    FILE *fp = fopen(f_profile.c_str(), "r");
     if (fp == NULL) {
-        cerr << "Trouble opening file " << k_path << "." << endl;
-        exit(EXIT_FAILURE);
+        cerr << "Trouble opening file " << f_profile << "." << endl;
+        assert(false);
     }
 
-    // 3 columns in each line.
+    FILE *fa = fopen(f_allowed.c_str(), "r");
+    if (fa == NULL) {
+        cerr << "Trouble opening file " << f_allowed << "." << endl;
+        assert(false);
+    }
+
     struct Column {
         char* c_str;
         size_t size;
@@ -150,9 +194,50 @@ void bit_load(const char* k_path, vector<KmerData>& result) {
             }
             return *s == '\0';
         }
+        inline bool is_snp_id() {
+            return (
+                (len >= 8) &&
+                (len <= 16) &&
+                is_numeric() &&
+                ((c_str[6] == '0') || (c_str[6] == '1'))
+            );
+        }
     };
 
-    Column cols[] = {
+    // columns in the DDDDDD.sckmer_allowed.tsv file.
+    Column allowed_cols[] = {
+        Column("kmer", '\t'),
+        Column("snp_id", '\n')
+    };
+    auto &ac_kmer = allowed_cols[0];
+    auto &ac_snp = allowed_cols[1];
+
+    uint64_t allowed_count = 0;
+    while (true) {
+        bool no_column_is_empty = true;
+        for (auto& c : allowed_cols) {
+            c.read_from_file(fa);
+            no_column_is_empty = no_column_is_empty && c.len > 0;
+        }
+        if (allowed_cols[0].len < 0) {
+            // end of file
+            break;
+        }
+        assert(no_column_is_empty && "Empty columnns are not allowed.");
+        assert(ac_kmer.len == K && "Kmer column must contain exactly K characters");
+        assert(ac_snp.is_snp_id() && "SNP id column needs to be decimal with at 8...16 digits and the character at position 6 must be '0' or '1'.");
+        auto kmer = seq_encode<K>(ac_kmer.c_str);
+        auto snp = strtoull(ac_snp.c_str, NULL, 10);
+        result.push_back(KmerData(kmer, snp));  // the snp will be amended later with more information
+        ++allowed_count;
+    }
+    fclose(fa);
+    auto end = result.end();
+    auto start = end - allowed_count;
+    sort(start, end);
+
+    // columns in the DDDDDD.sckmer_profiles.tsv file.
+    Column profile_cols[] = {
         Column("snp_genomic_position", '\t'),
         Column("snp_offset_in_forward_kmer", '\t'),
         Column("forward_kmer_major_allele", '\t'),
@@ -169,23 +254,24 @@ void bit_load(const char* k_path, vector<KmerData>& result) {
 
     auto kmer_columns = {2, 3, 4, 5};
 
-    auto& c_snp_genomic_position = cols[0];
-    auto& c_snp_offset_in_forward_kmer = cols[1];
-    auto& c_genome_id = cols[9];
+    auto& c_snp_genomic_position = profile_cols[0];
+    auto& c_snp_offset_in_forward_kmer = profile_cols[1];
+    auto& c_genome_id = profile_cols[9];
 
+    profiles.clear();
     while (true) {
         bool no_column_is_empty = true;
-        for (auto& c : cols) {
+        for (auto& c : profile_cols) {
             c.read_from_file(fp);
             no_column_is_empty = no_column_is_empty && c.len > 0;
         }
-        if (cols[0].len < 0) {
+        if (profile_cols[0].len < 0) {
             // end of file
             break;
         }
         assert(no_column_is_empty && "Empty columnns are not allowed.");
         for (auto kc : kmer_columns) {
-            assert(cols[kc].len == K && "Kmer column must contain exactly K characters");
+            assert(profile_cols[kc].len == K && "Kmer column must contain exactly K characters");
         }
         assert(c_snp_genomic_position.len <= 9 &&
                c_snp_genomic_position.is_numeric() &&
@@ -209,31 +295,50 @@ void bit_load(const char* k_path, vector<KmerData>& result) {
             assert(offset < (1ULL << 7));
             // Note that just from the snp_with_rc_and_offset we could recover the kmer if we have the reference genome.
             const uint64_t snp_with_rc_and_offset = (snp_coord << 8) | (reverse_complement << 7) | offset;
-            auto kmer = seq_encode<K>(cols[kc].c_str);
-            result.push_back(KmerData(kmer, snp_with_rc_and_offset));
+            auto kmer = seq_encode<K>(profile_cols[kc].c_str);
+            profiles.push_back(KmerData(kmer, snp_with_rc_and_offset));
         }
     }
+    fclose(fp);
+    sort(profiles.begin(), profiles.end());
+
+    if (profiles.size() == 0) {
+        cerr << "Empty file " << f_profile << endl;
+        assert(false && "Empty sckmer_profiles file.");
+    }
+
+    if (allowed_count == 0) {
+        cerr << "Empty file " << f_allowed << endl;
+        assert(false && "Empty sckmer_allowed file.");
+    }
+
+    // left-join "profiles" into "allowed" results, taking advantage of matching sort order
+    auto p = profiles.begin() - 1;
+    for (auto a = start; a != end; ++a) {
+        const auto akmer = get<0>(*a);
+        const auto asnp = get<1>(*a);
+        uint64_t pkmer, psnp;
+        do {
+            ++p;
+            assert((p != profiles.end()) && "sckmer_profiles not superset of sckmer_allowed");
+            pkmer = get_kmer(*p);
+            psnp = get_snp(*p);
+            assert(((pkmer < akmer) || ((pkmer == akmer) && (psnp <= asnp))) && "sckmer_profiles not superset of sckmer_allowed");
+        }
+        while ((akmer != pkmer) || (asnp != psnp));
+        get<1>(*a) = get<1>(*p);
+    }
+
+    cerr << (string("Loaded ") + to_string(allowed_count) + " kmers from files " + f_allowed + " and " + f_profile + " in " + to_string((chrono_time() - t_start) / 1000.0) + " secs.\n");
 }
 
-inline uint64_t get_kmer(const KmerData& kmer_data) {
-    return get<0>(kmer_data);
-}
-
-inline uint64_t get_snp_with_rc_and_offset(const KmerData& kmer_data) {
-    return get<1>(kmer_data);
-}
-
-inline uint64_t get_snp(const KmerData& kmer_data) {
-    return get<1>(kmer_data) >> 8;
-}
-
-uint64_t get_species(const KmerData& kmer_data) {
+uint64_t get_species(const KmerData& kd) {
     // We want the most signifficant 6 digits.  The number of digits to begin with is at most 16.
     // Here is a slow way to do it:
     //     x = stoi(to_string(get_snp(kmer_data)).substr(0, 6));
     // The fast way is below.  This actually makes a big difference in overall perf.
-    auto x = get_snp(kmer_data);
     // if it has 13 or more digits, remove the last 7
+    auto x = get_snp(kd);
     if (x >= 1000000000000) { // 10**12
         x /= 10000000;        // 10**7
     }
@@ -252,17 +357,12 @@ void multi_btc64(int n_path, const char** kpaths) {
 
     auto timeit = chrono_time();
 
-    if (n_path == 0) {
-        cerr << "No paths specified on command line --> will read from /dev/stdin." << endl;
-        n_path = 1;
-        const char* stdin = "/dev/stdin";
-        kpaths = &stdin;
-    }
+    assert(n_path > 0);
 
     // Just enough threads to be able to saturate input I/O, but not too many so
     // the merging of results in memory doesn't kill us.  In practice this produces
     // anywhere between 2x and 4x speedup, depending on the available I/O bandwidth.
-    constexpr auto MAX_THREADS = 12;
+    constexpr auto MAX_THREADS = 24;
     int num_running = 0;
     mutex mtx;
 
@@ -273,19 +373,22 @@ void multi_btc64(int n_path, const char** kpaths) {
 
     vector<KmerData> kdb;
     vector<KmerData>* kkdb[MAX_THREADS];
+    vector<KmerData>* scratch[MAX_THREADS];
     kkdb[0] = &kdb;
     for (int i = 1;  i < MAX_THREADS;  ++i) {
         kkdb[i] = new vector<KmerData>();
     }
+    for (int i = 0;  i < MAX_THREADS;  ++i) {
+        scratch[i] = new vector<KmerData>();
+        scratch[i]->resize(3000000);
+    }
 
     // This function runs in each thread.  It loads the bits of an input file,
     // then appends those bits to the kdb vector.
-    auto thread_func = [&num_running, &running, &mtx, &kkdb](const char* input_file_path, int thread_id) {
+    auto thread_func = [&num_running, &running, &mtx, &kkdb, &scratch](const char* input_file_path, int thread_id) {
         assert(0 <= thread_id && thread_id < MAX_THREADS);
-        auto t_start = chrono_time();
-        bit_load(input_file_path, *(kkdb[thread_id]));
+        bit_load(input_file_path, *(kkdb[thread_id]), *(scratch[thread_id]));
         mtx.lock(); {
-            cerr << "Loaded file " << input_file_path << " in " << (chrono_time() - t_start) / 1000.0 << " secs." << endl;
             --num_running;
             running[thread_id] = false;
         }
@@ -293,7 +396,7 @@ void multi_btc64(int n_path, const char** kpaths) {
     };
 
     // Wait for available thread slot (out of MAX_THREADS), and return its thread_id.
-    auto wait_for_thread_slot = [&num_running, &running, &mtx](const char* input_file_path = NULL) -> int {
+    auto wait_for_thread_slot = [&num_running, &running, &mtx]() -> int {
         bool thread_slot_is_available = false;
         int thread_id = -1;
         while (!(thread_slot_is_available)) {
@@ -301,9 +404,6 @@ void multi_btc64(int n_path, const char** kpaths) {
                 thread_slot_is_available = (num_running < MAX_THREADS);
                 if (thread_slot_is_available) {
                     ++num_running;
-                    if (input_file_path) {
-                        cerr << "Loading file " << input_file_path << "." << endl;
-                    }
                     for(int i = 0;  i < MAX_THREADS;  ++i) {
                         if (!(running[i])) {
                             running[i] = true;
@@ -323,7 +423,7 @@ void multi_btc64(int n_path, const char** kpaths) {
     };
 
     for (int i = 0; i < n_path; ++i) {
-        auto thread_id = wait_for_thread_slot(kpaths[i]);
+        auto thread_id = wait_for_thread_slot();
         thread(thread_func, kpaths[i], thread_id).detach();
     }
 
@@ -337,6 +437,10 @@ void multi_btc64(int n_path, const char** kpaths) {
     // similar to page table address translation.
     auto time_merge = chrono_time();
     cerr << "Merging input data from " << MAX_THREADS << " threads." << endl;
+
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        delete scratch[i];
+    }
 
     // note *kkdb[0] is an alias for kdb
     for (int i = 1; i < MAX_THREADS; ++i) {
@@ -359,6 +463,7 @@ void multi_btc64(int n_path, const char** kpaths) {
         using iterator = vector<KmerData>::iterator;
         auto sorter = [](iterator start, iterator end) {
             sort(start, end);
+
         };
         auto merger = [](iterator start, iterator middle, iterator end) {
             // LOL, turns out this "inplace_merge" isn't actually in-place, so it allocs tons of RAM!
@@ -429,14 +534,22 @@ void multi_btc64(int n_path, const char** kpaths) {
     cerr << "Purging conflicts removed " <<  multispecies_kmers << " multispecies kmers (" << multispecies_unique_kmers << " unique)." << endl;
 
     fh.close();
+
+    if (multispecies_kmers) {
+        cerr << "ERROR:  Multispecies kmers found!   There is possibly a problem with the upstream sckmerdb_allowed.tsv generator.\n";
+        assert(false && "multispecies kmers present in input");
+    }
 }
 
 void display_usage(const char *fname){
-    cout << "usage: " << fname << " fpath [fpath ...]\n";
+    cout << "usage: " << fname << " fpath [fpath ...]\n"
+         << "\n"
+         << "where fpath looks like DDDDDD.sckmer_allowed.tsv, for some 6-digit species id DDDDDD, and\n"
+         << "a matching file DDDDDD.sckmer_profiles.tsv must exist in the same place for the same DDDDDD.\n";
 }
 
 int main(int argc, const char** argv){
-    if (argc == 2 && (string(argv[1]) == "-h")) {
+    if ((argc == 2 && (string(argv[1]) == "-h")) || (argc <= 1)) {
         display_usage(argv[0]);
     } else {
         multi_btc64(argc - 1, argv + 1);
